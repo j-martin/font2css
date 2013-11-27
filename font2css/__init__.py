5325117293b359b5765f31133b46b4c95624d421
Jean-Martin <git@jmartin.ca>
2013-11-27 13:23:03 -0800

    initial commit

--- font2css/__init__.py
+++ font2css/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""font2css.py: Embeddeds font files in CSS.
+"""font2css.py: Embeds font files in CSS.
 
 Walks through directories (and sub directories), encode TTF and OTF files
 with base64 and save the result to a CSS file.
